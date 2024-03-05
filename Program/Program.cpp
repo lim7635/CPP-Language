94fa67e93097e66c766086cb12c4fc2e62605962
lim7635 <ju7635@naver.com>
2024-03-05 21:41:19 +0900

    2024.03.05 연관 컨테이너

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,50 +1,8 @@
 ﻿#include <iostream>
-#include <stack>
-#include <queue>
 using namespace std;
 
 int main()
 {
-#pragma region 컨테이너 어댑터
-	// 기존 컨테이너의 인터페이스를 제한하여
-	// 만든 기능이 제한되거나 변형된 컨테이너입니다.
-
-#pragma region Stack(LIFO) 선입후출
-
-	/*stack<int> stack;
-
-	stack.push(10);
-	stack.push(20);
-	stack.push(30);
-	stack.push(40);
-	stack.push(50);
-
-	while (stack.empty() == false)
-	{
-		cout << "Stack의 Top : " << stack.top() << endl;
-		stack.pop();
-	}*/
-
-#pragma endregion
-
-#pragma region Queue(FIFO) 선입선출
-
-	/*queue<int> queue;
-
-	queue.push(10);
-	queue.push(20);
-	queue.push(30);
-	queue.push(40);
-
-	while (queue.size())
-	{
-		cout << queue.front() << endl;
-		queue.pop();
-	}*/
-
-#pragma endregion
-
-#pragma endregion
 
 	return 0;
 }
